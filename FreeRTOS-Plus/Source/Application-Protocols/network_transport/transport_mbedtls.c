1d0bbe1214c96b6945fafc3add694234ac5fbaa3
Christian Jensen <tajen@users.noreply.github.com>
2026-04-22 10:00:28 +0200

    Added timeout to SSL handshake in transport_mbedtls.c

--- FreeRTOS-Plus/Source/Application-Protocols/network_transport/transport_mbedtls.c
+++ FreeRTOS-Plus/Source/Application-Protocols/network_transport/transport_mbedtls.c
@@ -599,11 +599,15 @@ static TlsTransportStatus_t tlsHandshake( NetworkContext_t * pNetworkContext,
     if( returnStatus == TLS_TRANSPORT_SUCCESS )
     {
         /* Perform the TLS handshake. */
+		TickType_t handshakeTimeoutTicks = pdMS_TO_TICKS(ipconfigSOCK_DEFAULT_RECEIVE_BLOCK_TIME);	
+		TimeOut_t handshakeStart;
+		vTaskSetTimeOutState(&handshakeStart);
         do
         {
             mbedtlsError = mbedtls_ssl_handshake( &( pTlsTransportParams->sslContext.context ) );
-        } while( ( mbedtlsError == MBEDTLS_ERR_SSL_WANT_READ ) ||
-                 ( mbedtlsError == MBEDTLS_ERR_SSL_WANT_WRITE ) );
+		} while(((mbedtlsError == MBEDTLS_ERR_SSL_WANT_READ) || 
+                 (mbedtlsError == MBEDTLS_ERR_SSL_WANT_WRITE))	&& 
+                 (xTaskCheckForTimeOut(&handshakeStart, &handshakeTimeoutTicks) == pdFALSE));
 
         if( mbedtlsError != 0 )
         {
